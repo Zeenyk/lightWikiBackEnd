251cc4524e3307c5a3914f22b8a5e778c65e2dfb
Zeenyk <federico05pese@gmail.com>
2025-10-04 20:12:44 +0200

    Modified lib.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# lightWikiBackEnd
-ciao
+# LightWiki BackEnd
+## OLLAMA
+Run the following command to install the correct OLLAMA model
+```
+ollama pull mxbai-embed-large
+```
---
+++ lib.py
@@ -0,0 +1,30 @@
+import os
+import ollama
+import struct
+from openai import OpenAI
+from dotenv import load_dotenv
+from scipy.spatial.distance import euclidean
+
+# * import openai key saved on .env file
+load_dotenv()
+client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"), base_url="https://api.deepseek.com")
+
+# * function to return a "blobbed" embedding of the sentence
+def get_blob(sentence):
+    embedding = ollama.embeddings(model='mxbai-embed-large', prompt=sentence)["embedding"]
+    blob = struct.pack(f'I{len(embedding)}f', len(embedding), *embedding)
+
+    return blob
+
+def blob2embedding(embedding_blob):
+    length = struct.unpack('I', embedding_blob[:4])[0]
+    embedding = list(struct.unpack(f'{length}f', embedding_blob[4:]))
+
+    return embedding
+
+def get_distance(blob_a, blob_b):
+    embedding_a = blob2embedding(blob_a)
+    embedding_b = blob2embedding(blob_b)
+
+    return euclidean(embedding_a, embedding_b) 
+
