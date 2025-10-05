986d66a7002994fdd0ee1c0e59ec62612c0e7a79
Zeenyk <federico05pese@gmail.com>
2025-10-05 05:45:08 +0200

    Added db.php

--- lib.php
+++ lib.php
@@ -3,6 +3,8 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+require_once __DIR__ . "../core/db.php";
+
 class EmbeddingAPI {
     private $db;
     private $pythonScriptPath;
